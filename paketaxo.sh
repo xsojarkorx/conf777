f4ba9529dc76a2a36f59845db77ece7f0c1382f6
xsojarkorx <32145031+xsojarkorx@users.noreply.github.com>
2021-06-03 20:44:31 -0500

    Create paketaxo.sh

---
+++ paketaxo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#Script post-install for Debian 10
+#
+#
+echo 'Agarra unas papitas en lo que termino'
+sleep 2s;
+#
+#
+sudo apt update && sudo apt upgrade
+#
+# Paquetes base
+sudo apt install xorg openbox obmenu obconf tint2 nitrogen network-manager-gnome clipit lxappearance thunar xfce4-terminal lxtask volumeicon-alsa alsa-utils build-essential dunst moc geany iceweasel
+#
+#
