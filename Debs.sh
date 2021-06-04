05c1ed94fd28b7c7f7cd2525e4fc92b6f51136af
xsojarkorx <32145031+xsojarkorx@users.noreply.github.com>
2021-06-03 19:45:49 -0500

    Update Debs.sh

--- Debs.sh
+++ Debs.sh
@@ -4,21 +4,14 @@
 #
 #Script para Debian 10
 #
-  echo -e '    Agarrate unas papitas y una coca\n     Acuerdate de agregar el contrib y non-free en los repos xd'
-  sudo apt update -y;
-  sudo apt upgrade -y;
-  #  Base
-  sudo apt install xorg wget openbox obmenu obconf nitrogen network-manager-gnome clipit lxxppearance thunar xfce4-terminal lxtask tint2 perl cpanminus volumeicon-alsa alsa-utils gcc build-essential dunst;
-  #  Editor de texto
-  sudo apt install geany;
-  #  Multimedia
-  sudo apt install vlc moc;
-  #  Internet  
-  sudo apt install iceweasel;
-  #  Imagen
-  mkdir -p ~/.config/openbox;
-  cp /etc/xdg/openbox/{rc.xml,menu.xml,environment} ~/.config/openbox;
-  cd ~/.config/openbox;
-  echo -e 'nitrogen --restore &\ntint2 &\nvolumeicon &\nnm-applet &\nclipit &\' > autostart ;
-  clear;
-  }
+echo -e '    Agarrate unas papitas y una coca\n     Acuerdate de agregar el contrib y non-free en los repos xd'
+sleep 2s
+#  Base
+sudo apt-get install xorg wget openbox obmenu obconf nitrogen network-manager-gnome clipit lxappearance thunar xfce4-terminal lxtask tint2 perl cpanminus volumeicon-alsa alsa-utils gcc build-essential dunst;
+#  Editor de texto
+sudo apt-get install geany;
+#  Multimedia
+sudo apt-get install vlc moc;
+#  Internet  
+sudo apt-get install iceweasel;
+#
